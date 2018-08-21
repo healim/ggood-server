36091b97e25fd4e1de61b14da7d025fd3f36a33a
healim <healim.me@gmail.com>
2018-08-21 12:19:46 +0900

    - 서버 실행을 위한 라이브러리 설치, scripts 작성 - eslint 적용 - `.gitignore` 업데이트 (환경변수 설정)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
+const path = require('path')
+
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "settings": {
+        "import/resolver": {
+            node: {
+                paths: [path.resolve('./src')]
+            }
+        }
+    },
+    "rules": {
+        "no-unused-vars": 0,
+        "comma-dangle": 0,
+        "eol-last": 0,
+        "no-console": 0,
+        "semi": ["error", "never"]
+    }
 };
